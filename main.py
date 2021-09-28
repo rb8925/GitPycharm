b50dddd4773044a4748264a734e160d25e4ae089
rb8925 <71216149+rb8925@users.noreply.github.com>
2021-09-28 14:40:23 +0900

    Updates on Local Master Branch

--- main.py
+++ main.py
@@ -18,3 +18,6 @@ print("Hello World 5")
 
 #adding new item in main branch of Github
 print("Hello World Update on Github branch")
+
+#adding new item in master branch
+print("Hello World Updates on Local Master Branch")
